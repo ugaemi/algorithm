0bb92c240bdf8d260934b17d190f19daba5d152a
ugaemi <u.gaemi@gmail.com>
2020-01-25 21:12:06 +0900

    hackerrank - ArrayManipulation

--- hackerrank/array-manipulation.py
+++ hackerrank/array-manipulation.py
@@ -1,12 +1,17 @@
 def arrayManipulation(n, queries):
-    last_array = [0] * n
+    result = [0] * (n + 1)
+    max_value = -1
     for query in queries:
         start = query[0] - 1
         end = query[1]
-        for i in range(start, end):
-            last_array[start] += query[2]
-            start += 1
-    return max(last_array)
+        result[start] += query[2]
+        result[end] += -query[2]
+    tmp = 0
+    for r in result:
+        tmp += r
+        if max_value < tmp:
+            max_value = tmp
+    return max_value
 
 
 if __name__ == '__main__':
