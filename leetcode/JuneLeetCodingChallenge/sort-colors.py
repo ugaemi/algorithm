fd7bd1709b01d018210e56c8cd67a2fc04ca97a0
ugaemi <u.gaemi@gmail.com>
2020-06-15 21:19:04 +0900

    Leetcode - June Challenge week3

---
+++ leetcode/JuneLeetCodingChallenge/sort-colors.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
