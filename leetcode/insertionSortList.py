a221b55e8fbdc9d934f7c5c398cedf67978dba07
ugaemi <u.gaemi@gmail.com>
2020-11-02 22:59:22 +0900

    Leetcode - November Challenge week1

---
+++ leetcode/insertionSortList.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        cur = dummy = ListNode(0)
+        while head:
+            if cur and cur.val > head.val:
+                cur = dummy
+            while cur.next and cur.next.val < head.val:
+                cur = cur.next
+            cur.next, cur.next.next, head = head, cur.next, head.next
+        return dummy.next
