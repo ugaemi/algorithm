48b992055f34c3ba62db3c417cb4aaf55e26e222
ugaemi <u.gaemi@gmail.com>
2021-07-04 17:10:06 +0900

    pythonAlgorithmInterview: isPalindrome

--- pythonAlgorithmInterview/06/isPalindrome.py
+++ pythonAlgorithmInterview/06/isPalindrome.py
@@ -1,3 +1,5 @@
+import collections
+import re
 import unittest
 
 
@@ -11,6 +13,21 @@ class Solution:
             if stack.pop(0) != stack.pop():
                 return False
         return True
+
+    # def isPalindrome(self, s: str) -> bool:
+    #     deque = collections.deque()
+    #     for c in s:
+    #         if c.isalnum():
+    #             deque.append(c.lower())
+    #     while len(deque) > 1:
+    #         if deque.popleft() != deque.pop():
+    #             return False
+    #     return True
+
+    # def isPalindrome(self, s: str) -> bool:
+    #     s = s.lower()
+    #     s = re.sub('[^a-z0-9]', '', s)
+    #     return s == s[::-1]
 
 
 class Test(unittest.TestCase):
