d4a74c7c16c533a67c45686dfda7b35e2b0651b9
ugaemi <u.gaemi@gmail.com>
2021-07-20 21:48:31 +0900

    LeetCode: trappingRainWater

--- leetcode/trappingRainWater.py
+++ leetcode/trappingRainWater.py
@@ -4,19 +4,23 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
+        stack = []
         volume = 0
-        left, right = 0, len(height) - 1
-        left_max, right_max = height[left], height[right]
-        while left < right:
-            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
-            if left_max <= right_max:
-                volume += left_max - height[left]
-                left += 1
-            else:
-                volume += right_max - height[right]
-                right -= 1
+
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+
+                if not len(stack):
+                    break
+
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+
+            stack.append(i)
+
         return volume
 
 
