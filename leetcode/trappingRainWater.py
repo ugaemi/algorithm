498f546e95e13ce8e60429f917fd1f8e19b809b3
ugaemi <u.gaemi@gmail.com>
2021-07-20 21:39:01 +0900

    LeetCode: twoSum, trappingRainWater

---
+++ leetcode/trappingRainWater.py
@@ -0,0 +1,31 @@
+import unittest
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        volume = 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        while left < right:
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1
+        return volume
+
+
+class Test(unittest.TestCase):
+    def test_trap(self):
+        solution = Solution()
+        self.assertEqual(solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6)
+        self.assertEqual(solution.trap([4, 2, 0, 3, 2, 5]), 9)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- leetcode/twoSum.py
+++ leetcode/twoSum.py
@@ -1,13 +1,23 @@
+import unittest
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        nums_map = {}
+        for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target - num], i]
+            nums_map[num] = i
 
 
 class Test(unittest.TestCase):
-    pass
+    def test_twoSum(self):
+        solution = Solution()
+        self.assertEqual(solution.twoSum([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(solution.twoSum([3, 2, 4], 6), [1, 2])
+        self.assertEqual(solution.twoSum([3, 3], 6), [0, 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
