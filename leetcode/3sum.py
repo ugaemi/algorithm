21cefa733a824de21c54222700c9af0b67fef36e
ugaemi <u.gaemi@gmail.com>
2021-07-21 15:04:27 +0900

    LeetCode: 3sum

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -1,52 +1,41 @@
-import collections
+import unittest
 from typing import List
 
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        res = []
-        if len(nums) < 3:
-            return res
-        for i in range(len(nums)):
-            needs = 0 - nums[i]
-            for j in range(i+1, len(nums)):
-                need = needs - nums[j]
-                tmp_nums = nums[:]
-                tmp_nums.remove(nums[i])
-                tmp_nums.remove(nums[j])
-                if need in tmp_nums:
-                    answer = [nums[i], nums[j], need]
-                    if sorted(answer) not in res:
-                        res.append(answer)
-        return res
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+
+        return results
 
 
-# Best example
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         res = []
-#         nums.sort()
-#         length = len(nums)
-#
-#         for i in range(length - 2):
-#             if nums[i] > 0:
-#                 break
-#             if i > 0 and nums[i] == nums[i - 1]:
-#                 continue
-#             l, r = i + 1, length - 1
-#             while l < r:
-#                 total = nums[i] + nums[l] + nums[r]
-#                 if total < 0:
-#                     l += 1
-#                 elif total > 0:
-#                     r -= 1
-#                 else:
-#                     res.append([nums[i], nums[l], nums[r]])
-#                     while l < r and nums[l] == nums[l + 1]:
-#                         l += 1
-#                     while l < r and nums[r] == nums[r - 1]:
-#                         r -= 1
-#                     l += 1
-#                     r -= 1
-#         return res
+class Test(unittest.TestCase):
+    def test_threeSum(self):
+        solution = Solution()
+        self.assertEqual(solution.threeSum([-1, 0, 1, 2, -1, -4]), [[-1, -1, 2], [-1, 0, 1]])
+        self.assertEqual(solution.threeSum([]), [])
+        self.assertEqual(solution.threeSum([0]), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
