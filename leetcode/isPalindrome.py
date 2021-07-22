dc876bc18180f28cc173f6cdae255c2f12d7436b
ugaemi <u.gaemi@gmail.com>
2021-07-22 21:05:39 +0900

    LeetCode: isPalindrome

---
+++ leetcode/isPalindrome.py
@@ -0,0 +1,27 @@
+import collections
+from typing import Deque
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        q: Deque = collections.deque()
+
+        if not head:
+            return True
+
+        node = head
+        while node:
+            q.append(node.val)
+            node = node.next
+
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+
+        return True
