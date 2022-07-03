5c803da9ff98339e0e6aaaa9a4ec3046ac72997b
ugaemi <u.gaemi@gmail.com>
2022-07-03 18:17:25 +0900

    leetcode: merge k sorted lists

---
+++ leetcode/merge-k-sorted-lists.py
@@ -0,0 +1,21 @@
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        self.nodes = []
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for x in sorted(self.nodes):
+            point.next = ListNode(x)
+            point = point.next
+        return head.next
