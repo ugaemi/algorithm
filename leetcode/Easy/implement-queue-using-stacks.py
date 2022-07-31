7f2274a659ed544fcacf1b88cbb3dc14a3b0773c
ugaemi <u.gaemi@gmail.com>
2022-07-31 22:18:31 +0900

    leetcode: Implement Queue Using Stacks

---
+++ leetcode/Easy/implement-queue-using-stacks.py
@@ -0,0 +1,47 @@
+"""
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+"""
+import unittest
+
+
+class MyQueue:
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def push(self, x: int) -> None:
+        self.input.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.output.pop()
+
+    def peek(self) -> int:
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self) -> bool:
+        return self.input == [] and self.output == []
+
+
+class Test(unittest.TestCase):
+    def test_MyQueue(self):
+        my_queue = MyQueue()
+        my_queue.push(1)
+        my_queue.push(2)
+        self.assertEqual(my_queue.peek(), 1)
+        self.assertEqual(my_queue.pop(), 1)
+        self.assertEqual(my_queue.empty(), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
