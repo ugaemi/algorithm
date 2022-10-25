42955da88dbdb1cbcad1f219644f1c727fd1337e
ugaemi <u.gaemi@gmail.com>
2022-10-25 23:37:02 +0900

    leetcode: Two Sum

---
+++ leetcode/Easy/two_sum.py
@@ -0,0 +1,21 @@
+import unittest
+from typing import List
+
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    nums_map = {}
+    for i, num in enumerate(nums):
+        if target - num in nums_map:
+            return [nums_map[target - num], i]
+        nums_map[num] = i
+
+
+class Test(unittest.TestCase):
+    def test_two_sum(self):
+        self.assertEqual(two_sum([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(two_sum([3, 2, 4], 6), [1, 2])
+        self.assertEqual(two_sum([3, 3], 6), [0, 1])
+
+
+if __name__ == "__main__":
+    unittest.main()
