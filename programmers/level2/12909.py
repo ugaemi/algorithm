aeb0130680a552dd5a95c45288e9314a07fe418c
ugaemi <u.gaemi@gmail.com>
2022-11-08 23:08:13 +0900

    programemrs: 올바른 괄호

---
+++ programmers/level2/12909.py
@@ -0,0 +1,35 @@
+"""
+괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+"()()" 또는 "(())()" 는 올바른 괄호입니다.
+")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+"""
+import unittest
+
+
+def valid_parenthesis(s: str) -> bool:
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        else:
+            try:
+                stack.pop()
+            except IndexError:
+                return False
+    if stack:
+        return False
+    return True
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self.assertEqual(valid_parenthesis("()()"), True)
+        self.assertEqual(valid_parenthesis("(())()"), True)
+        self.assertEqual(valid_parenthesis(")()("), False)
+        self.assertEqual(valid_parenthesis("(()("), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
