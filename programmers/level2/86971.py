7c040a0020fd3e8b2edafd769b436e904650aa5b
ugaemi <u.gaemi@gmail.com>
2022-12-20 11:17:32 +0900

    programmers: 전력망을 둘로 나누기

--- programmers/level2/86971.py
+++ programmers/level2/86971.py
@@ -5,7 +5,6 @@ from collections import deque
 def bfs(node, visited, tree):
     queue = deque([node])
     cnt = 1
-    visited[node] = True
     while queue:
         now = queue.popleft()
         for i in tree[now]:
@@ -23,11 +22,11 @@ def power_grid(n, wires):
         tree[v1].append(v2)
         tree[v2].append(v1)
     for node1, node2 in wires:
-        visited = [False] * (n + 1)
+        visited = [True] + [False] * n
+        visited[node1] = True
         visited[node2] = True
         result = bfs(node1, visited, tree)
-        if abs(result - (n - result)) < answer:
-            answer = abs(result - (n - result))
+        answer = min(answer, abs(result - (n - result)))
     return answer
 
 
@@ -36,3 +35,7 @@ class PowerGridTest(unittest.TestCase):
         self.assertEqual(power_grid(9, [[1, 3], [2, 3], [3, 4], [4, 5], [4, 6], [4, 7], [7, 8], [7, 9]]), 3)
         self.assertEqual(power_grid(4, [[1, 2], [2, 3], [3, 4]]), 0)
         self.assertEqual(power_grid(7, [[1, 2], [2, 7], [3, 7], [3, 4], [4, 5], [6, 7]]), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
